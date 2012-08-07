9235d76254c17e5f679c41381dacea7e705344e3
bfahy <bfahy@andrew.cmu.edu>
2012-08-07 19:26:05 -0400

    intial commit. Working on example script

---
+++ readinput.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+import sys
+import os
+
+bold = "\033[1;37m"
+reset = "\033[0;0m"
+queues = ("red","blue","cyan","green","magenta")
+
+def choices(c):
+    return dict(enumerate(c))
+
+
+def selectchoices(c,default=None,startnum=0):
+
+    if default and default not in c:
+        raise Exception("default choice not in choices")
+    
+    if default:
+        prompt = "please select\n"
+        for x in enumerate(c,start=startnum):
+            if x[1] == default:
+                prompt += bold
+                prompt += "[%d] %s default\n" % x
+                prompt += reset
+            else:
+                prompt += "[%d] %s \n" % x
+        # prompt = ("please select\n" + "\n".join(["[%d] %s default" % x if x[1] == default
+        #                                         else "[%d] %s " % x
+        #                                         for x in enumerate(c)]) + "\n")
+    else:
+        prompt = ("please select\n" + "\n".join(["[%d] %s" % x for x in enumerate(c)]) + "\n:")
+    
+    dchoices = dict(enumerate(c,start=startnum))
+    while True:
+        sys.stdout.write(prompt)
+        choice = raw_input().lower()
+        if choice == "exit":
+            raise Exception('exit')
+        elif default is not None and choice == '':
+            return default
+        elif choice in c:
+            return choice
+        elif choice in dchoices:
+            return dchoices[choice]
+        else:
+            try:
+                intchoice = int(choice)
+            except ValueError:
+                sys.stdout.write("Invalid selection %s \n" % choice )
+            else:
+                if intchoice in dchoices:
+                    return dchoices[intchoice]
+                else:
+                    sys.stdout.write("Invalid: selection %d out of range \n" % intchoice )
+    # End select choices
+                    
+def askqueue(defaultqueue="red"):
+    queue = selectchoices(queues,default=defaultqueue)
+    sys.stdout.write("selected %s queue\n" % queue)
+    return queue
+    #return selectchoices(queues)
+
+
+def askrange(start=1,end=1,defaultval=1):
+    return selectchoices(list(range(start,end)),default=defaultval,startnum=start)
+
+    
+def askdir(description,default=os.getcwd()):
+    prompt = "Enter directory for %s \n (%s):" % (description,default)
+    
+    while True:
+        sys.stdout.write(prompt)
+        userdir = raw_input()
+        if userdir == "exit":
+            raise Exception('exit')
+        elif  userdir == '' and os.path.isdir(default):
+            return default
+        elif os.path.isdir(default):
+            return default
+        elif os.path.isdir(userdir):
+            return userdir
+        else:
+            sys.stdout.write("INVALID direcotry %s\n" % userdir)
+            
+def askstring(description,default=""):
+    prompt = "Enter directory for %s \n (%s):" % (description,default)
+    while True:
+        sys.stdout.write(prompt)
+        userstring = raw_input()
+        if userstring == "exit":
+            raise Exception('exit')
+        elif userstring == '' and default:
+            return default
+        elif userstring:
+            return userstring
+        else:
+            sys.stdout.write("INVALID entry %s\n" % userstring)
+            
+def askyesno(description,default=True):
+    if default:
+        prompt = "%s [Y]/n " % description
+    else:
+        prompt = "%s y/[N] " % description
+    while True:
+        ans = raw_input(prompt)
+        if not ans:
+            return default
+        if ans.lower() not in ['y', 'yes', 'n', 'no']:
+            print 'please enter y or n.'
+            continue
+        if ans.lower() == 'y' or ans == 'yes':
+            return True
+        if ans.lower() == 'n' or ans == 'no':
+            return False
+
+def readgeom(default=[1,1,1,1]):
+    while True:
+        ans = raw_input("Input gemoetry ('%d %d %d %d')" % tuple(default))
+        if not ans:
+            return default
+        try:
+            x,y,z,t = ans.split(' ')
+            if not all(map(String.isdigit,ans.split(' '))):
+                print "not valid numbers"
+                continue
+        except ValueError:
+            print 'invalid form for geom must be like "1 1 1 2"'
+            continue
+        return map(int,ans.split(' '))
+            
+
+if __name__ == '__main__':
+    print askqueue()
+    # selected = selectchoices(queues)
+    # print "selected thing was %s" % selected
