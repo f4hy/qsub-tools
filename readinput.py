a9a41f4fa89d835b330d5c62872dd17522d2df49
bfahy <bfahy@andrew.cmu.edu>
2012-08-28 17:04:15 -0400

    Style change to match pep8

--- readinput.py
+++ readinput.py
@@ -4,20 +4,21 @@ import os
 
 bold = "\033[1;37m"
 reset = "\033[0;0m"
-queues = ("red","blue","cyan","green","magenta")
+queues = ("red", "blue", "cyan", "green", "magenta")
+
 
 def choices(c):
     return dict(enumerate(c))
 
 
-def selectchoices(c,default=None,startnum=0):
+def selectchoices(c, default=None, startnum=0):
 
     if default and default not in c:
         raise Exception("default choice %s not in choices" % str(default))
-    
+
     if default:
         prompt = "please select\n"
-        for x in enumerate(c,start=startnum):
+        for x in enumerate(c, start=startnum):
             if x[1] == default:
                 prompt += bold
                 prompt += "[%d] %s default\n" % x
@@ -29,8 +30,8 @@ def selectchoices(c,default=None,startnum=0):
         #                                         for x in enumerate(c)]) + "\n")
     else:
         prompt = ("please select\n" + "\n".join(["[%d] %s" % x for x in enumerate(c)]) + "\n:")
-    
-    dchoices = dict(enumerate(c,start=startnum))
+
+    dchoices = dict(enumerate(c, start=startnum))
     while True:
         sys.stdout.write(prompt)
         choice = raw_input().lower()
@@ -46,29 +47,30 @@ def selectchoices(c,default=None,startnum=0):
             try:
                 intchoice = int(choice)
             except ValueError:
-                sys.stdout.write("Invalid selection %s \n" % choice )
+                sys.stdout.write("Invalid selection %s \n" % choice)
             else:
                 if intchoice in dchoices:
                     return dchoices[intchoice]
                 else:
-                    sys.stdout.write("Invalid: selection %d out of range \n" % intchoice )
+                    sys.stdout.write("Invalid: selection %d out of range \n" % intchoice)
     # End select choices
-                    
+
+
 def askqueue(defaultqueue="red"):
-    queue = selectchoices(queues,default=defaultqueue)
+    queue = selectchoices(queues, default=defaultqueue)
     sys.stdout.write("selected %s queue\n" % queue)
     return queue
     #return selectchoices(queues)
 
 
-def askrange(start=1,end=1,defaultval=1):
-    defaultval = min(max(start,defaultval),end)     # make sure default is in the range
-    return selectchoices(list(range(start,end+1)),default=defaultval,startnum=start)
+def askrange(start=1, end=1, defaultval=1):
+    defaultval = min(max(start, defaultval), end)     # make sure default is in the range
+    return selectchoices(list(range(start, end + 1)), default=defaultval, startnum=start)
 
-    
-def askdir(description,default=os.getcwd()):
-    prompt = "Enter directory for %s \n (%s):" % (description,default)
-    
+
+def askdir(description, default=os.getcwd()):
+    prompt = "Enter directory for %s \n (%s):" % (description, default)
+
     while True:
         sys.stdout.write(prompt)
         userdir = raw_input()
@@ -82,9 +84,10 @@ def askdir(description,default=os.getcwd()):
             return userdir
         else:
             sys.stdout.write("INVALID direcotry %s\n" % userdir)
-            
-def askstring(description,default=""):
-    prompt = "Enter directory for %s \n (%s):" % (description,default)
+
+
+def askstring(description, default=""):
+    prompt = "Enter directory for %s \n (%s):" % (description, default)
     while True:
         sys.stdout.write(prompt)
         userstring = raw_input()
@@ -96,8 +99,9 @@ def askstring(description,default=""):
             return userstring
         else:
             sys.stdout.write("INVALID entry %s\n" % userstring)
-            
-def askyesno(description,default=True):
+
+
+def askyesno(description, default=True):
     if default:
         prompt = "%s [Y]/n " % description
     else:
@@ -114,23 +118,24 @@ def askyesno(description,default=True):
         if ans.lower() == 'n' or ans == 'no':
             return False
 
-def readgeom(default=[1,1,1,1]):
+
+def readgeom(default=[1, 1, 1, 1]):
     while True:
         ans = raw_input("Input gemoetry ('%d %d %d %d')" % tuple(default))
         if not ans:
             return default
         try:
-            x,y,z,t = ans.split(' ')
-            print x,y,z,t
+            x, y, z, t = ans.split(' ')
+            print x, y, z, t
             print ans.split(' ')
-            if not all( s.isdigit() for s in ans.split(' ')):
+            if not all(s.isdigit() for s in ans.split(' ')):
                 print "not valid numbers"
                 continue
         except ValueError:
             print 'invalid form for geom must be like "1 1 1 2"'
             continue
-        return map(int,ans.split(' '))
-            
+        return map(int, ans.split(' '))
+
 
 if __name__ == '__main__':
     print askqueue()
--- sub.py
+++ sub.py
@@ -9,10 +9,12 @@ import readinput
 import os
 from subprocess import call
 
+
 def usage():
     print """Usage:
     sub.py submitscript.sh"""
     exit(1)
+
 
 def read(filename):
     print sys.argv
@@ -20,47 +22,46 @@ def read(filename):
     print "reading %s" % filename
     file = open(filename)
     filetext = file.read()
-    
+
     # find #PBS -N gaugeandmeasurerun
-    matchname = re.search('#PBS -N (.*)',filetext)
+    matchname = re.search('#PBS -N (.*)', filetext)
     name = matchname.group(1)
     # find #PBS -l nodes=4:ppn=8
-    dimensions = re.search('#PBS -l nodes=(\d+):ppn=(\d+)',filetext)
+    dimensions = re.search('#PBS -l nodes=(\d+):ppn=(\d+)', filetext)
     nodes = (int)(dimensions.group(1))
     ppn = (int)(dimensions.group(2))
 
     # find #PBS -q blue
-    matchqueue = re.search('#PBS -q (.+)',filetext)
+    matchqueue = re.search('#PBS -q (.+)', filetext)
     queue = matchqueue.group(1)
 
     # find GEOM="1 1 1 8"
-    matchgeom = re.search('GEOM="(\d+) (\d+) (\d+) (\d+)"',filetext)
-    geom = [int(matchgeom.group(i)) for i in range(1,5)]
+    matchgeom = re.search('GEOM="(\d+) (\d+) (\d+) (\d+)"', filetext)
+    geom = [int(matchgeom.group(i)) for i in range(1, 5)]
 
     # find PERMDIR="/latticeQCD/raid3/bfahy/develop/gaugeandmeasure/iso"
-    matchpermdir = re.search('PERMDIR="(.+)"',filetext)
+    matchpermdir = re.search('PERMDIR="(.+)"', filetext)
     permdir = matchpermdir.group(1)
 
     print "Name %s" % name
-    print "Nodes %d, PPN %d" % (nodes,ppn)
-    print "Queue %s" %queue
+    print "Nodes %d, PPN %d" % (nodes, ppn)
+    print "Queue %s" % queue
     print geom
     print "permdir %s" % permdir
 
-    return (name,nodes,ppn,queue,geom,permdir)
-    
+    return (name, nodes, ppn, queue, geom, permdir)
 
-def write(defaults,filename):
+
+def write(defaults, filename):
     file = open(filename)
     filetext = file.read()
     file.close()
 
-    name,nodes,ppn,queue,geom,permdir=defaults
-    
-    
-    name = readinput.askstring("Set job name",name)
+    name, nodes, ppn, queue, geom, permdir = defaults
 
-    queue =readinput.askqueue() 
+    name = readinput.askstring("Set job name", name)
+
+    queue = readinput.askqueue()
 
     if queue == "red":
         noderange = 12
@@ -74,56 +75,45 @@ def write(defaults,filename):
     sys.stdout.write("select number of nodes (was %d)\n" % nodes)
 
     #nodes = readinput.selectchoices(list(range(1,noderange+1)),default=nodes,startnum=1)
-    nodes = readinput.askrange(1,noderange,nodes)
+    nodes = readinput.askrange(1, noderange, nodes)
 
-
-    if readinput.askyesno("Set permdir to current dir?\n (%s)" %os.getcwd()):
+    if readinput.askyesno("Set permdir to current dir?\n (%s)" % os.getcwd()):
         permdir = os.getcwd()
     else:
-        permdir = readinput.askdir("Set perm directory",permdir)
+        permdir = readinput.askdir("Set perm directory", permdir)
 
-
-    optimalgeom = [1,1,1,sockets*nodes]
+    optimalgeom = [1, 1, 1, sockets * nodes]
 
     if readinput.askyesno("use optimal GEOM=%d,%d,%d,%d" % tuple(optimalgeom)):
         geom = optimalgeom
     else:
         geom = readinput.readgeom(geom)
 
-    filetext = re.sub('#PBS -N (.*)','#PBS -N %s'%name,filetext)
-    filetext = re.sub('#PBS -l nodes=(\d+):ppn=(\d+)',"#PBS -l nodes=%d:ppn=%d" % (nodes,ppn),filetext)
-    filetext = re.sub('#PBS -q (.+)',"#PBS -q %s" %queue,filetext)
-    filetext = re.sub('GEOM="(\d+) (\d+) (\d+) (\d+)"','GEOM="%d %d %d %d"' % tuple(geom),filetext)
-    filetext = re.sub('PERMDIR="(.+)"','PERMDIR="%s"'%permdir,filetext)
+    filetext = re.sub('#PBS -N (.*)', '#PBS -N %s' % name, filetext)
+    filetext = re.sub('#PBS -l nodes=(\d+):ppn=(\d+)', "#PBS -l nodes=%d:ppn=%d" % (nodes, ppn), filetext)
+    filetext = re.sub('#PBS -q (.+)', "#PBS -q %s" % queue, filetext)
+    filetext = re.sub('GEOM="(\d+) (\d+) (\d+) (\d+)"', 'GEOM="%d %d %d %d"' % tuple(geom), filetext)
+    filetext = re.sub('PERMDIR="(.+)"', 'PERMDIR="%s"' % permdir, filetext)
 
     print filetext
 
     newfilename = filename + ".tosub"
     print "writing %s" % newfilename
 
-    outfile=open(newfilename,'w')
+    outfile = open(newfilename, 'w')
     outfile.write(filetext)
     outfile.flush()
     outfile.close()
 
-    
     if os.uname()[1] == 'erwin':
         if readinput.askyesno("submit %s" % newfilename):
-            print "qsubing %s" %newfilename
-            call(["qsub",newfilename])
+            print "qsubing %s" % newfilename
+            call(["qsub", newfilename])
 
-        
-    if readinput.askyesno("move %s to %s" %(newfilename,filename),False):
-        print "moving %s to %s" % (newfilename,filename)
-        call(["mv",newfilename,filename])
-        
-    
-    
-    #readinput.selectchoices(['a'])
-    
-    
-    
-    
+    if readinput.askyesno("move %s to %s" % (newfilename, filename), False):
+        print "moving %s to %s" % (newfilename, filename)
+        call(["mv", newfilename, filename])
+
 if len(sys.argv) != 2:
     usage()
 
@@ -131,6 +121,6 @@ filename = sys.argv[1]
 
 defaults = read(filename)
 
-write(defaults,filename)
+write(defaults, filename)
 
 print "done"
