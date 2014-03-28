a81598f34386c3e5af37b23a316c5c17b46cff8d
Brendan Fahy <bmfahy@gmail.com>
2014-03-27 20:23:19 -0400

    single command qsubs and now depend on each other

--- qsub_command.py
+++ qsub_command.py
@@ -31,6 +31,7 @@ def main():
     parser = argparse.ArgumentParser(description="Submit a job to do a single command")
     parser.add_argument("command", type=str, help="command to execute")
     parser.add_argument("-n", "--name", type=str, default="singlecommand", help="set the name of the job")
+    parser.add_argument("-W", "--wait", type=str, action="append", required=False, help="have job depend upon annother")
     parser.add_argument("-q", "--queue", default="red", choices=["green", "blue", "cyan", "magenta", "red"], help="queue to submit to")
     parser.add_argument("-o", "--options", required=False, action="append", type=str, help="options to pass to qsub")
     args = parser.parse_args()
@@ -38,12 +39,15 @@ def main():
 
     tmpfile = NamedTemporaryFile()
     tmpfile.write(header.format(NAME=args.name, QUEUE=args.queue))
+    if args.wait:
+        tmpfile.write("#PBS -W depend=afterok:{}\n".format(":".join(args.wait)))
     tmpfile.write("cd /scratch/PBS_${PBS_JOBID}\n")
     tmpfile.write("echo {}".format(args.command))
     tmpfile.write("\n")
     tmpfile.write(args.command)
     tmpfile.write("\n")
     tmpfile.flush()
+    #call(["cat", tmpfile.name])
     if args.options:
         exelist = ["/opt/pbs/bin/qsub", tmpfile.name]
         exelist[1:1] = ["-l "+ o for o in args.options]
